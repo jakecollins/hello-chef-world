b32a49901e9955e3331c6558508c34f1d386dc28
Jake Collins <jakcollins@expedia.com>
2015-02-05 08:25:12 +0000

    Example 2.1 - Start using environments.

---
+++ environments/vagrant.rb
@@ -0,0 +1,6 @@
+# Defines the default attributes for a Chef environment. These will replace corresponding default attributes in Chef cookbooks. 
+default_attributes :helloworld => {
+  :message => 'hello chef world ruby environment message',
+  :messages => ['environment message one', 'environment message two'],
+  :properties => { :key1 => 'environment value 1', :key2 => 'environment value 2'}
+}
