e6e0aa9f4c2eb12e7689f28e88400290a7710f90
Jake Collins <jakcollins@expedia.com>
2015-02-05 08:39:20 +0000

    Example 3 - Start using roles.

--- environments/vagrant.rb
+++ environments/vagrant.rb
@@ -1,4 +1,6 @@
 # Defines the default attributes for a Chef environment. These will replace corresponding default attributes in Chef cookbooks. 
+# See http://docs.opscode.com/essentials_environments.html
+
 default_attributes :helloworld => {
   :message => 'hello chef world ruby environment message',
   :messages => ['environment message one', 'environment message two'],
