bd2c1e00a8815cbd98317c0d3fb77299b7df96d4
Jake Collins <jakcollins@expedia.com>
2015-02-05 08:41:03 +0000

    Example 3.1 - Start using roles.

---
+++ roles/hellorole.rb
@@ -0,0 +1,8 @@
+# Roles can add recipes or other roles to the run list. They can also replace the default attributes from the Chef environment and/or cookbooks.  
+# See http://docs.opscode.com/essentials_roles.html
+
+run_list 'recipe[helloworld::list]', 'recipe[helloworld::map]'
+default_attributes :helloworld => {
+  :messages => ['role message one', 'role message two'],
+  :properties => { :key3 => 'role value 3', :key4 => 'role value 4'}
+}
