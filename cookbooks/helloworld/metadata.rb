1a76bbcfbbfb4ce80be752915e275884b9aa29d6
Jake Collins <jakcollins@expedia.com>
2016-04-19 15:09:05 +0100

    Example 5.1 - Start using Berkshelf.

--- cookbooks/helloworld/metadata.rb
+++ cookbooks/helloworld/metadata.rb
@@ -1,2 +1,5 @@
 # Defines metadata for the helloworld cookbook.
 name 'helloworld'
+
+# Declare a dependency on the apt cookbook, see https://supermarket.chef.io/cookbooks/apt
+depends 'apt', '~> 3.0' # Pessimistically lock to minor version, see https://docs.chef.io/cookbook_versions.html
